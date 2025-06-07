d3836f85a7df71bc81ada980b433e7ad8c6e263c
NekoTom12343 <tommeo2014@gmail.com>
2025-06-08 02:24:19 +0700

    update: input list of junctions_id and edge_id + random accident in edge or junction

--- src/accident_manager.py
+++ src/accident_manager.py
@@ -3,16 +3,18 @@ import random
 from shapely.geometry import Point, Polygon
 
 class AccidentManager:
-    def __init__(self, junction_id, start_step, duration=1000):
+    def __init__(self,  start_step, duration=1000, junction_id_list = [], edge_id_list = [],):
         """
         Initialize the AccidentManager class.
 
         Args:
-            junction_id (str): ID of the junction where the accident occurs.
+            junction_id (list): List ID of the junction where the accident may occurs.
+            edge_id (list ): List ID of the edge where the accident occurs.
             start_step (int): Step at which the accident should start.
             duration (int): Duration for which the accident lasts.
         """
-        self.junction_id = junction_id
+        self.junction_id_list = junction_id_list
+        self.edge_id_list = edge_id_list
         self.start_step = start_step
         self.duration = duration
         self.accident_active = False
@@ -26,23 +28,53 @@ class AccidentManager:
             int: Number of vehicles on the specified junction.
             list: List of vehicle IDs on the junction.
         """
-        junction_shape = Polygon(traci.junction.getShape(self.junction_id))
-        vehicles_inside_count = 0
+        if not self.junction_id_list:
+            print("No junction IDs provided.")
+            return 0, []
         vehicle_ids_in_junction = []
-        vehicle_ids = traci.vehicle.getIDList()
+        total_vehicles_count = 0
+        for junction_id in self.junction_id_list:
+            junction_shape = Polygon(traci.junction.getShape(junction_id))
+            vehicle_ids = traci.vehicle.getIDList()
 
-        for vehicle_id in vehicle_ids:
-            vehicle_position = traci.vehicle.getPosition(vehicle_id)
-            vehicle_point = Point(vehicle_position)
-            if junction_shape.contains(vehicle_point):
-                vehicles_inside_count += 1
-                vehicle_ids_in_junction.append(vehicle_id)
+            for vehicle_id in vehicle_ids:
+                vehicle_position = traci.vehicle.getPosition(vehicle_id)
+                vehicle_point = Point(vehicle_position)
+                if junction_shape.contains(vehicle_point):
+                    total_vehicles_count += 1
+                    vehicle_ids_in_junction.append(vehicle_id)
 
-        return vehicles_inside_count, vehicle_ids_in_junction
-
-    def random_stop_vehicle_in_junction(self, vehicle_ids):
+        return total_vehicles_count, vehicle_ids_in_junction
+    
+    def count_vehicles_on_edge(self):
         """
-        Randomly stops a vehicle in the junction.
+        Counts the number of vehicles on specific edges.
+
+        Args:
+            edge_id (list): ID of the edge to count vehicles on.
+
+        Returns:
+            int: Number of vehicles on the specified edge.
+            list: List of vehicle IDs on the edge.    
+        """
+        if not self.edge_id_list:
+            print("No edge IDs provided.")
+            return 0, []
+        vehicle_ids_in_edge_list = []
+        total_vehicle_count = 0
+        for edge_id in self.edge_id_list:
+            vehicle_count = traci.edge.getLastStepVehicleNumber(edge_id)
+            total_vehicle_count += vehicle_count
+            vehicle_ids_in_edge = traci.edge.getLastStepVehicleIDs(edge_id)
+            vehicle_ids_in_edge_list.extend(vehicle_ids_in_edge)
+        # print(vehicle_ids_in_edge_list)
+        # print(f"Type of vehicle_ids_in_edge_list: {type(vehicle_ids_in_edge_list)}")
+
+        return total_vehicle_count, vehicle_ids_in_edge_list
+    
+    def random_stop_vehicle(self, vehicle_ids):
+        """
+        Randomly stops a vehicle in vehicle list.
 
         Args:
             vehicle_ids (list): List of vehicle IDs to choose from.
@@ -75,13 +107,13 @@ class AccidentManager:
             current_step (int): Current simulation step.
         """
         if current_step == self.start_step and not self.accident_active:
-            vehicle_list = self.count_vehicles_on_junction()[1]
+            vehicle_list = self.count_vehicles_on_junction()[1] + self.count_vehicles_on_edge()[1]
             if vehicle_list:
-                self.stopped_vehicle = self.random_stop_vehicle_in_junction(vehicle_list)
+                self.stopped_vehicle = self.random_stop_vehicle(vehicle_list)
                 self.accident_active = True
-                print(f"Accident started at step {current_step} in junction {self.junction_id}.")
+                print(f"Accident started at step {current_step}.")
 
         if current_step == self.start_step + self.duration and self.accident_active:
             self.remove_stopped_vehicle()
             self.accident_active = False
-            print(f"Accident ended at step {current_step} in junction {self.junction_id}.")
+            print(f"Accident ended at step {current_step}.")
